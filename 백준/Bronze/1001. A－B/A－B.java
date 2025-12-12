d294eb3b4f4ad981d185deac7e0020d7601d0ac4
dameun0527 <jisuryu0527@gmail.com>
2025-12-12 13:00:01 +0900

    [Bronze V] Title: A-B, Time: 104 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        
+        int A = Integer.parseInt(input[0]);
+        int B = Integer.parseInt(input[1]);
+        
+        System.out.println(A - B);
+    }
+}
