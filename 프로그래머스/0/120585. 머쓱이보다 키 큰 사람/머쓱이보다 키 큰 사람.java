302e802200667a1ff3800faaae02cb9215c82b4c
dameun0527 <jisuryu0527@gmail.com>
2025-12-15 14:14:55 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 1.51 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] array, int height) {
+        int answer = (int)Arrays.stream(array)
+            .filter(n -> n > height)
+            .count();
+        return answer;
+    }
+}
